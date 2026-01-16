3d59dc798c408da4247d4b274e0101395ad81560
Arpan Bhattacharya <arpanbht.ofc@gmail.com>
2026-01-16 13:03:09 +0530

    Update success message for GET /data endpoint

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/", (_, res) => {
 
 // TEST endpoints
 app.get("/data", (_, res) => {
-  res.status(200).json({ message: "get retrieved successfully", data: temp });
+  res.status(200).json({ message: "Data retrieved successfully", data: temp });
 });
 
 app.post("/data", (req, res) => {
